224203cd8eba5b0adbd87c9bd33246e94a221cb3
Agnes Muita <agnes.muita@alternateltd.com>
2022-05-10 03:10:31 +0300

    write transactions to json, and validations

---
+++ transaction.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const addTransaction = function(title, token, timestamp){
+  const transactions = loadTransactions();  
+  const duplicateTransaction = transactions.filter(function(transaction)
+    {
+      return transaction.timestamp === timestamp;
+    });
+    if(duplicateTransaction.length==0){
+      transactions.push({title:title,token:token,timestamp:timestamp})
+      saveTransaction(transactions)
+      console.log("New transaction added successfully")
+    }
+    else{
+      console.log("Transaction already added")
+    }
+}
+
+//loadTransactions returns an array
+const loadTransactions = function()
+{
+  try{
+      const dataBuffer = fs.readFileSync('transactions.json');
+      const dataJSON = dataBuffer.toString(); 
+      return JSON.parse(dataJSON)
+  }
+  catch(e){
+      return []
+  }
+}
+
+//save transaction
+const saveTransaction=function(transactions){
+  const dataJSON = JSON.stringify(transactions)
+  fs.writeFileSync('transactions.json',dataJSON)
+}
+
+module.exports = {
+  addTransaction:addTransaction
+}
